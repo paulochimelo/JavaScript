cc5e7bd68c15e16a332e974d7a3b095241290ab2
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2020-05-01 19:57:37 -0300

    Create for2.js

---
+++ Controle/for2.js
@@ -0,0 +1,16 @@
+const notas = [9, 8, 4, 5, 7, 2]
+
+for (let i in notas){
+    console.log(i, notas[i])
+}
+
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idade: 29,
+    peso: 64
+}
+
+for(let atributo in pessoa){
+    console.log(`${atributo} = ${pessoa[atributo]} `)
+}
