dcfb80f62c0e57e029973cacd35c25c0b427bc37
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2020-05-02 18:49:54 -0300

    Create desafioFuncContrutora.js

---
+++ Funcao/desafioFuncContrutora.js
@@ -0,0 +1,10 @@
+function pessoa(nome){
+    this.nome = nome    
+
+    this.falar= function(){
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new pessoa('Paulo')
+p1.falar()
