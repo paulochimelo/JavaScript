d227a0d1c04d47af02bf7d17256d920c2b9a2c56
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2020-05-02 16:11:00 -0300

    Create tiposDeclaracao.js

---
+++ Funcao/tiposDeclaracao.js
@@ -0,0 +1,15 @@
+
+// function declaration - forma tradicional - o interpretador carrega essas funçoes antes de executar o codigo
+function soma (x, y){
+    return x + y
+}
+
+//function expression 
+const sub = function(x, y){
+    return x - y
+}
+
+//Named function expression
+const mult = function mult(x, y){
+    return x * y
+}
