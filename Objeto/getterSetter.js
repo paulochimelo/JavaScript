eee830f3771f2b463d6e695b3d7fd108afea453e
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2020-11-07 23:26:48 -0300

    07/11

---
+++ Objeto/getterSetter.js
@@ -0,0 +1,16 @@
+const sequecia = {
+    _valor: 1, //convenção
+    get valor() {return this._valor++},
+    set valor(valor) {
+        if(valor>this._valor){
+            this._valor = valor
+        }
+    }
+}
+
+
+console.log(sequecia.valor, sequecia.valor)
+sequecia.valor = 1000
+console.log(sequecia.valor, sequecia.valor)
+sequecia.valor = 900
+console.log(sequecia.valor, sequecia.valor)
